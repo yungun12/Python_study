8a81b32f55b053f96708721f129a4fc15449dd8a
yungun12 <yungun200512@gmail.com>
2024-10-17 08:58:21 +0900

    명화 감상 프로그램

---
+++ window7-2/Code10.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+
+# 함수 선언 부분
+def func_open():
+    filename = askopenfilename(parent=window,
+                               filetypes=(("GIF 파일", "*.gif"),
+                                          ("모든 파일", "*.*")))
+    if filename:  # 파일이 선택되었는지 확인
+        photo = PhotoImage(file=filename)
+        pLabel.configure(image=photo)
+        pLabel.image = photo  # 참조 유지
+
+def func_exit():
+    window.quit()
+    window.destroy()
+
+# 메인 코드 부분
+window = Tk()
+window.geometry('400x400')
+window.title("명화 감상하기")
+
+photo = PhotoImage()
+pLabel = Label(window, image=photo)
+pLabel.pack(expand=1, anchor=CENTER)
+
+mainMenu = Menu(window)
+window.config(menu=mainMenu)
+fileMenu = Menu(mainMenu)
+mainMenu.add_cascade(label="파일", menu=fileMenu)
+fileMenu.add_command(label="파일 열기", command=func_open)
+fileMenu.add_separator()
+fileMenu.add_command(label="프로그램 종료", command=func_exit)
+
+window.mainloop()
