2e91892f8a78abd35ec0f65ad82ee6daa283e1d9
yungun12 <yungun200512@gmail.com>
2024-11-05 10:45:24 +0900

    위젯 고정 위치에 배치

--- window7-1/Code1-11.py
+++ window7-1/Code1-11.py
@@ -1,27 +1,25 @@
-#code1-11
-
 from tkinter import *
 
-#전역 변수 선언 부분
+# 전역 변수 선언 부분
 btnList = [None] * 9
-fnameList = ["froyo.gif", "gingerbread.gif","honeycomb.gif","icecream.gif,"
-            "jellybean.gif","kitkat.gif","lollipop.gif","marshmallow.gif","nougat.gif"]
+fnameList = ["froyo.gif", "gingerbread.gif", "honeycomb.gif", "icecream.gif",
+             "jellybean.gif", "kitkat.gif", "lollipop.gif", "marshmallow.gif", "nougat.gif"]
 photoList = [None] * 9
 i, k = 0, 0
 xPos, yPos = 0, 0
 num = 0
 
-#메인 코드 부분
+# 메인 코드 부분
 window = Tk()
 window.geometry("210x210")
 
-for i in range(0, 9) :
-    photoList[i] = PhotoImage(file = "gif/" + fnameList[i])
-    btnList[i] = Button(window, image = photoList[i])
+for i in range(0, 9):
+    photoList[i] = PhotoImage(file="gif/" + fnameList[i])
+    btnList[i] = Button(window, image=photoList[i])
 
-for i in range(0, 3) :
-    for k in range(0, 3) :
-        btnList[num].place(x = xPos, y = yPos)
+for i in range(0, 3):
+    for k in range(0, 3):
+        btnList[num].place(x=xPos, y=yPos)
         num += 1
         xPos += 70
     xPos = 0
