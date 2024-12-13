c2e424e8117f182d8425aca4af89815915323b7c
윤건 <yungun200512@gmail.com>
2024-12-13 13:43:29 +0900

    클래스2

---
+++ bigdata5-1/adder2.py
@@ -0,0 +1,17 @@
+#adder2.py
+from bigdata5.adder1 import result
+
+result1 = 0
+result2 = 0
+def adder1(num):
+    global result1
+    result1 += num
+    return result1
+def adder2(num):
+    global result2
+    result2 += num
+    return result2
+print(adder1(3))
+print(adder1(4))
+print(adder2(3))
+print(adder2(7))
